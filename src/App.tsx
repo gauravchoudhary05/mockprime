692bbd5797efbeec26e53aff9397107f2e86eeb4
Gaurav Choudhary <gauravchoudhary05@gmail.com>
2025-12-25 19:45:30 +0530

    REMOVE missing toaster/sonner imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { Toaster } from "./components/ui/toaster";
-import { Toaster as Sonner } from "./components/ui/sonner";
-import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
@@ -22,7 +19,6 @@ const queryClient = new QueryClient();
 const App = () => (
   <div className="min-h-screen w-full overflow-x-hidden bg-background">
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
         <AuthProvider>
           <Toaster />
           <Sonner />
@@ -42,7 +38,6 @@ const App = () => (
             </Routes>
           </BrowserRouter>
         </AuthProvider>
-      </TooltipProvider>
     </QueryClientProvider>
   </div>
 );
