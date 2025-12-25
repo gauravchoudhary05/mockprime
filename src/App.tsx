5b8c25c654d61bd8d7344ce1732b4c90714f2dcc
Gaurav Choudhary <gauravchoudhary05@gmail.com>
2025-12-25 20:00:55 +0530

    MINIMAL App.tsx - No external deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
@@ -13,32 +12,24 @@ import Solutions from "./pages/Solutions";
 import LegalPage from "./pages/LegalPage";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <div className="min-h-screen w-full overflow-x-hidden bg-background">
-    <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/auth" element={<Auth />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/mock-tests" element={<MockTests />} />
-              <Route path="/practice" element={<Practice />} />
-              <Route path="/test/:testName" element={<Test />} />
-              <Route path="/result" element={<Result />} />
-              <Route path="/upgrade" element={<Upgrade />} />
-              <Route path="/solutions/:testName" element={<Solutions />} />
-              <Route path="/legal" element={<LegalPage />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </AuthProvider>
-    </QueryClientProvider>
-  </div>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/mock-tests" element={<MockTests />} />
+        <Route path="/practice" element={<Practice />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/upgrade" element={<Upgrade />} />
+        <Route path="/solutions" element={<Solutions />} />
+        <Route path="/legal" element={<LegalPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
